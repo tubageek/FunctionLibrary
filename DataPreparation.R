844f0066746c89d27b0a3eb13369ebbb591818e0
Scott Norberg <scottnorberg@gmail.com>
2016-04-30 08:45:17 -0400

    Added train/test split

--- DataPreparation.R
+++ DataPreparation.R
@@ -1,4 +1,7 @@
-random.split <- function (dataset, ratio) {
+random.split <- function (dataset, ratio = 0.8, seed = 412) {
   #TODO: check for valid data in "ratio"
-  
+  trainingSize <- floor(ratio * nrow(dataset))
+  set.seed(seed)
+  trainingIndexes <- sample(seq_len(nrow(dataset)), size = trainingSize)
+  list(train = dataset[trainingIndexes, ], test = dataset[-trainingIndexes, ])
 }
